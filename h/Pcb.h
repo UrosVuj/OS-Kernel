3a059b7e36f300c7f2020b7a0091a669836ac0bb
Uroš Vujošević <UrosVuj@users.noreply.github.com>
2020-01-19 09:38:34 +0100

    Code formatting, structuring, cleanup

--- h/Pcb.h
+++ h/Pcb.h
@@ -10,34 +10,28 @@ class PCB{
 
 public:
 
-
 	volatile static ID _id;
-	volatile static PCB* running;        //PCB of the running thread, global variable
-	volatile static int numberofthreads; // Number of threads that aren't finished
+	volatile static PCB* running; //PCB of the running thread, global variable
+	volatile static int numberofthreads; //Number of threads that aren't finished
 	volatile State state_thread;
 
 
 	unsigned sp;
 	unsigned ss;
 	unsigned bp;
-	volatile	unsigned ended; //1 if Thread ended its job, 0 otherwise
-	volatile	int time;       //Amount of time the thread can hold the CPU
+	volatile unsigned ended; //1 if Thread ended its job, 0 otherwise
+	volatile int time; //Amount of time the thread can hold the CPU
 	unsigned *stack;
 
-	volatile ID personal_id;  //ID of a certain thread
+	volatile ID personal_id; //ID of a certain thread
+
+	volatile PCB* Blocked_first; //pointer to the first in the list of blocked threads
+	volatile PCB* Blocked_last; //due to waitToComplete();
+
+	volatile PCB* next_blocked; //next in the waitToComplete() list, starts with 0
 
 
-
-	volatile PCB* Blocked_first;		//pointer to the first in the list of blocked threads
-	volatile PCB* Blocked_last;      //due to waitToComplete();
-
-	volatile PCB* next_blocked;            // next in the waitToComplete()
-   	   	   	   	   	   	   	   	   	   	  //list, starts with 0
-
-
-
-
-	volatile PCB* next_sem_blocked;       //fields used by the semaphore class
+	volatile PCB* next_sem_blocked; //fields used by the semaphore class
 	volatile Time timeleft_semblock;
 	volatile int signaled;
 	volatile int is_wait_0;
@@ -65,9 +59,11 @@ public:
 
 
     void waitToComplete();
-    void block_add();	//helper for waitToComplete()
-    					//for adding running thread to
-    					//the called threads blocked list
+    void block_add();
+	/*
+	helper for waitToComplete()for adding running 
+	thread to the called threads blocked list
+	*/
 
 
     void exitThread();
@@ -85,7 +81,7 @@ public:
 
     static void wrapper();
 
-			};
+};
 
 void idle();
 
