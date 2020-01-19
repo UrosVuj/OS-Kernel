0eb801e479ee0b73fb2311889c4ae9fdf34791bc
Uroš Vujošević <UrosVuj@users.noreply.github.com>
2020-01-19 08:55:07 +0100

    Formatting

--- src/Thread.cpp
+++ src/Thread.cpp
@@ -4,11 +4,15 @@
 #include <iostream.h>
 
 void Thread::start(){
+	
 	myPCB->start();
+
 }
 
 ID Thread::getId(){
+
 	return myPCB->getId();
+
 }
 
 Thread::~Thread(){
@@ -22,29 +26,35 @@ Thread::~Thread(){
 }
 
 void Thread::waitToComplete(){
+
 	myPCB->waitToComplete();
+
 }
 
 Thread* Thread::getThreadById(ID _id){
 	
-
 	PCB* temp = PCB::All_threads_first;
 	if(!temp) return 0;
 	while(temp && temp->personal_id!=_id)temp=temp->next;
 	if(temp)return temp->myThread;
 	else return 0;
 
-
 }
 
  ID Thread::getRunningId(){
+
 	return PCB::getRunningId();
-}
-void dispatch(){
-	real_dispatch();
+
 }
 
+void dispatch(){
+
+	real_dispatch();
+
+}
+
+//When you make a new Thread, it just makes a new PCB and links them
 Thread::Thread (StackSize stackSize, Time timeSlice){
-	myPCB = new PCB(this,stackSize,timeSlice);      //When you make a new Thread, it
-													//just makes a new PCB and links them
+
+	myPCB = new PCB(this,stackSize,timeSlice);
 }
